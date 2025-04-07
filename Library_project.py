8757991cd55e60f40766fcf9f8ca72f6378a54fe
Pl1zarik <121376270+Pl1zarik@users.noreply.github.com>
2025-04-07 23:49:36 +0300

    Create Library_project.py

---
+++ Library_project.py
@@ -0,0 +1,50 @@
+class Book():
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+    
+    # def info(self):
+    #     print'Название:', title,'Автор:', author,'Год', year)
+
+class fuction_library():
+    def __init__(self):
+        self.library = {}
+    
+    def add_book(self, title, author, year):
+        new_book = title(author, year)
+        self.library.append(new_book)
+        print('Новая книга', title ,'была добавлена')
+        return
+    
+    def remove_book(self, titel):
+        if titel in self.library:
+            del self.library[titel]
+        else:
+            print('Данная книга не найдена в библиотеке')
+        return
+    
+    def find_book(self, titel):
+        if titel in self.library:
+            print(self.library[titel])
+        else:
+            print('Данная книга не найдена в библиотеке')
+        return
+    
+    def show_books(self):
+        if len(self.library) == 0:
+            print('Книг пока нет')
+        else:
+            print(self.library)
+        return
+def Help():
+    print(
+        'Добавить книгу\nУдалить книгу\nНайти книгу\nПоказать все книги'
+    )
+book = input('Введите запрос (Help - все комманды):').lower()
+while book != 'стоп':
+    if book == 'Добавить книгу':
+        titel = input('Введите название').title()
+        author = input('Введите автора').title()
+        year = input('Введите год написания')
+        add_book(title, author, year)
